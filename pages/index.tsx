42ef7a000148fa6af6f4c61ecc13e9e44250dbe6
Otto A. Laitinen <otto.laitinen@mindler.fi>
2022-08-10 17:28:13 +0300

    feat: implement design wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 import { request } from "graphql-request";
 
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import useSWR from "swr";
 import useStore from "../util/store";
 import { useState } from "react";
@@ -14,7 +13,7 @@ const fetcher = (query: string, variables?: { stationId: string }) =>
     variables
   );
 
-const Home: NextPage = () => {
+const Dashboard: NextPage = () => {
   const currentStationId = useStore((state) => state.currentStationId);
   const changeCurrentStation = useStore((state) => state.changeCurrentStation);
 
@@ -41,9 +40,10 @@ const Home: NextPage = () => {
   );
 
   const station = data?.bikeRentalStation;
+  const availabilityPercentage = station?.bikesAvailable / station?.capacity;
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>{station?.name} City Bike Station</title>
         <meta
@@ -53,36 +53,53 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          {station?.name}{" "}
-          <a href="https://www.hsl.fi/en/citybikes">City Bike</a> Station
-        </h1>
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h2>
-              {station?.name} ({station?.stationId})
-            </h2>
-            <h3>Availability</h3>
-            <p>
-              {station?.bikesAvailable} / {station?.capacity}
+      <main className="flex flex-col justify-center items-center h-full">
+        <div className="grid grid-cols-2 h-full w-full">
+          <div className="flex flex-col justify-center items-start h-full p-12 space-y-2">
+            <h1 className="text-8xl font-semibold">Bike Watch</h1>
+            <p className="text-2xl font-light max-w-prose">
+              A web app to check how your closest city Helsinki region bike
+              station is doing. Designed to be used as dashboard.
+            </p>
+            <p className="justify-self-end font-light text-2xl max-w-prose">
+              A hobby project by Otto A. Laitinen
             </p>
           </div>
+          <div className="flex flex-col justify-center items-center h-full bg-yellow-500 ">
+            <div className="flex flex-col bg-white p-8 rounded-2xl space-y-4 min-w-[600px]">
+              <div className="flex flex-row space-x-4">
+                <div className="bg-yellow-500 rounded-lg h-20 w-20" />
+                <div className="flex flex-col">
+                  <h1 className="text-4xl">{station?.name}</h1>
+                  <p className="text-base font-light">
+                    Bike station {station?.stationId}
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-row space-x-3 align-start py-4">
+                <div className="text-8xl font-semibold ">
+                  {station?.bikesAvailable}
+                </div>
+                <div className="text-8xl">/</div>
+                <div className="text-8xl">{station?.capacity}</div>
+              </div>
+              <div className="w-full bg-gray-200 rounded-full h-6">
+                <div
+                  className="bg-green-600 h-6 rounded-full"
+                  style={{
+                    width: `${availabilityPercentage * 100}%`,
+                  }}
+                ></div>
+              </div>
+              <button className="text-zinc-600 text-lg font-semibold hover:text-opacity-75">
+                Change station
+              </button>
+            </div>
+          </div>
         </div>
-        <form onSubmit={() => changeCurrentStation(newStationId)}>
-          <label>
-            Change station (input id):
-            <input
-              type="text"
-              value={newStationId}
-              onChange={(event) => setNewStationId(event.target.value)}
-            />
-          </label>
-          <input type="submit" value="Change" />
-        </form>
       </main>
-    </div>
+    </>
   );
 };
 
-export default Home;
+export default Dashboard;
